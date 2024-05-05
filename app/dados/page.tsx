b9cb2440e98463763fcda362d901b0a54c7bd103
Ramon Guelber Martins <rgmguelber@gmail.com>
2024-05-05 15:56:11 -0300

    Carrega banco de dados

--- app/dados/page.tsx
+++ app/dados/page.tsx
@@ -1,3 +1,24 @@
-export default function Dados() {
-  return <h2 className="text-xl text-gray-500 italic mb-48">Dados</h2>;
+import { PrismaClient } from "@prisma/client";
+
+export default async function Dados() {
+  const prisma = new PrismaClient();
+
+  const caixas = await prisma.caixas.findMany();
+  prisma.$disconnect();
+
+  // const caixas
+  return (
+    <section className="w-full flex flex-col justify-center items-center mb-20">
+      <h2 className="text-xl text-gray-500 italic mb-48">Dados</h2>;
+      <table>
+        {caixas.map((caixa) => (
+          <tr key={caixa.CaixaID}>
+            <td className="w-20 text-center">{caixa.CaixaID}</td>
+            <td className="w-72 text-center">{caixa.Caixa}</td>
+            <td className="w-20 text-center">{caixa.SaldoAtual.toFixed(2)}</td>
+          </tr>
+        ))}
+      </table>
+    </section>
+  );
 }
